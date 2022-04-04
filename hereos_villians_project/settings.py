d77b3b4a28d0842a88d84ead18c945074f21f648
SASands <snads@live.com>
2022-04-04 15:02:47 -0400

    created localsettings file and moved sensitive data + updated databases default values

--- hereos_villians_project/settings.py
+++ hereos_villians_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import imp
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8267@3$!e9717saj(tyi3=k@tc7aa72a!@7hvd$5w@j#w-j*u0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,17 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hereos_villians_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -121,3 +108,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from hereos_villians_project.local_settings import *
+except ImportError:
+    pass
