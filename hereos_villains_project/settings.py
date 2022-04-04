5e924021dddcd78a1eddefc4a2cf5781809f6e2f
SASands <snads@live.com>
2022-04-04 15:54:47 -0400

    created supers and super_types app + created database in sql workbench + migrated + imported collection into postman

--- hereos_villains_project/settings.py
+++ hereos_villains_project/settings.py
@@ -110,6 +110,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from hereos_villians_project.local_settings import *
+    from hereos_villains_project.local_settings import *
 except ImportError:
     pass
